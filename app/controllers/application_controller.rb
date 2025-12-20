d79dc618fa1b80b767b39f4c12ebf70ebad2d4f8
leongswan <leongswan19860919@gmail.com>
2025-12-20 16:35:24 +0900

    feat: ユーザープロフィール編集機能（名前・画像）を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
+  # Deviseのコントローラーを使うときだけ、下の configure_... を実行する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
 
   def configure_permitted_parameters
-    # 新規登録時 (sign_up) に avatar も許可
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
-
-    # 編集時 (account_update) に avatar も許可
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+    # 新規登録(sign_up)と、アカウント更新(account_update)の時に、nameとavatarを許可する
+    keys = [:name, :avatar]
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: keys)
   end
 end
