89526794d66ac939d0a739fb02b9e7b7f873d10c
leongswan <leongswan19860919@gmail.com>
2025-12-22 01:09:06 +0900

    feat: ユーザーのプロフィール写真アップロード機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Deviseのコントローラーを使うときだけ、下の configure_... を実行する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    # 新規登録(sign_up)と、アカウント更新(account_update)の時に、nameとavatarを許可する
-    keys = [:name, :avatar]
-    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
-    devise_parameter_sanitizer.permit(:account_update, keys: keys)
+    # 新規登録(sign_up)のときに許可する項目
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role, :image])
+    
+    # プロフィール編集(account_update)のときに許可する項目
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role, :image, :profile])
   end
 end
