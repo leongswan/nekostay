7699b7a75c9280d34cca17c11433ebb425c952e7
leongswan <leongswan19860919@gmail.com>
2025-12-24 20:59:40 +0900

    feat: リアルタイムチャット機能の実装（Hotwire/Turbo Streams） & MVP完成！

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,10 @@ class Message < ApplicationRecord
 
   # ↓↓↓ これを追加（空のメッセージは禁止）
   validates :body, presence: true
+
+  # ★★★ ここが魔法の呪文です ★★★
+  # メッセージが作成(create)されたら、
+  # 紐づいている予約(@stay)のチャネルに向かって、
+  # "messages" というIDの場所に、自分自身(_message.html.erb)を追加(append)して放送する
+  after_create_commit -> { broadcast_append_to stay, target: "messages" }
 end
